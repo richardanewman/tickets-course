acfa9374383f4ab9cbc56d1985714f2f859dedb7
Rick Newman <rick@richardnewman.dev>
2022-11-25 23:06:20 -0500

    adds react/nextjs. configures client k8s.

---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+import { buildClient } from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/current-user');
+  return data;
+};
+
+export default LandingPage;
