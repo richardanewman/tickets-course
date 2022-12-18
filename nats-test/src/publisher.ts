557af627e3fa8d2f590f39f94947ebda755454cc
Rick Newman <rick@richardnewman.dev>
2022-12-18 18:04:09 -0500

    adds nats streaming to tickets and auth services.

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,32 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 33,
+      userId: 'test',
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
